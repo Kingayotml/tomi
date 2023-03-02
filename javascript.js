379a03b04041ee3253717446f21e6093f74209f9
Kingayotml <122526969+Kingayotml@users.noreply.github.com>
2023-03-02 17:20:28 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let str1 = 'Saturday night plans'
+console.log(str1.startsWith('Sat'))
+
+let str = 'Geeks for Geeks'
+console.log(str.startsWith('gee'))
+
+let string = 'It is a great day'
+console.log(string.startsWith('It'))
+
+let string1 = 'It is a sunny day'
+console.log(string1.startsWith('sunny'))
+
+// The two parameters of substr() are start and length , while for substring() , they are start and end . substr() 's start index will wrap to the end of the string if it is negative, while substring() will clamp it to 0 .
+
+let s = 'python'
+
